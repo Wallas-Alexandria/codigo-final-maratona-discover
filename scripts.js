24972f715d7a214d65f398d2a482069c04facfc6
Wallas Alexandria <alexandria.wallas@gmail.com>
2022-01-16 16:44:30 -0300

    correcao de bug

--- scripts.js
+++ scripts.js
@@ -117,8 +117,8 @@ const DOM = {
 const Utils = {
 
     formatAmount(value) {
-        value = Number(value) * 100
-        return value
+       value = value * 100
+       return Math.round(value)
     },
 
     formatDate(date) {
